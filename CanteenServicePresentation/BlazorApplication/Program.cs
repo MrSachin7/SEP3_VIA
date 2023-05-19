63bc8ac94a13dc058a458025886f0ef53239e960
MrSachin7 <82201769+MrSachin7@users.noreply.github.com>
2023-05-19 22:19:42 +0200

    add

--- CanteenServicePresentation/BlazorApplication/Program.cs
+++ CanteenServicePresentation/BlazorApplication/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using BlazorApplication.Data;
+using Contracts;
+using HttpClients;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +10,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+
+builder.Services.AddScoped<IUserService, UserHttpClient>();
+builder.Services.AddScoped<IReservationService, ReservationHttpClient>();
+builder.Services.AddScoped<IMenuService, MenuHttpClient>();
 
 var app = builder.Build();
 
